b207fb267ce76e53e2835cf15d188aa904e904ce
kimhyunso <gustn9515@gmail.com>
2023-02-23 15:18:06 +0900

    20230223 15:18

--- CrawlingModule.py
+++ CrawlingModule.py
@@ -7,9 +7,9 @@ class YoutubeBulider():
     __VERSION = 'v3'
     __video_id_list = list()
     __comment_list = list()
-    __category_list = list()
     __channel_id = ''
-    __views_count_list = list()
+    __videos_list = list()
+    __cate_gory_dict = dict()
 
     def __init__(self, api_key) -> None:
         '''
@@ -38,18 +38,23 @@ class YoutubeBulider():
         self.__key = api_key
         self.__youtube = build(self.__PLATFORM, self.__VERSION, developerKey=self.__key)
 
+        inline = open('./category_id_list.txt', 'r')
+        for line in inline.readlines():
+            key_value = line.split(' - ')
+            self.__cate_gory_dict[int(key_value[0])] = key_value[1][:-1]
+
 
     def get_videoId_in_channel(self, channelId:str, maxResults=50) -> list:
         self.__response = self.__youtube.search().list(part='id,snippet', channelId=channelId, type='video', maxResults=maxResults, order='date').execute()
         
         count = 0 
-
         while self.__response:
             for item in self.__response['items']:
                 self.__video_id_list.append(item['id']['videoId'])
 
             if count == 1:
                 break
+            
             if 'nextPageToken' in self.__response:
                 self.__response = self.__youtube.search().list(part='id,snippet', channelId=channelId, type='video', pageToken=self.__response['nextPageToken'], maxResults=maxResults, order='date').execute()
             count += 1
@@ -58,7 +63,14 @@ class YoutubeBulider():
 
 
     def search_channelId(self, search_name:str, maxResults=1) -> str:
+        '''
+        채널명을 토대로 channelId값을 알아온다.
+        Args:
+            
+        
+        '''
         self.__response = self.__youtube.search().list(part='id,snippet', q=search_name, type='channel', maxResults=maxResults).execute()
+
         for item in self.__response['items']:
             self.__channel_id = item['id']['channelId']
         return self.__channel_id
@@ -66,31 +78,25 @@ class YoutubeBulider():
     def get_categoryId_in_channel(self, videoId_list:list, regionCode='kr', maxResults=1) -> list:
         '''
         > return list
-        YoutubeAPI를 통해 인기급상승 영상ID(video_id)를 크롤링해온다.
         https://developers.google.com/youtube/v3/docs/videos/list
         Args:
-            **part : id, snippet, contentDetails, fileDetails, liveStreamingDetails, player, processingDetails, recordingDetails, statistics, status, suggestions, topicDetails - 필수매개변수
-            chart : mostPopular - 인기동영상
+            videoId_list : video_id를 받는다.
             regionCode : 이 매개변수를 사용하는 경우 차트 또한 설정해야 합니다. 이 매개변수 값은 ISO 3166-1 alpha-2 국가 코드
             maxResults : 몇 개의 결과를 반환받을 것인지
         Attributes:
             response : Videos에서 목록의 쿼리를 날려서 결과를 도출 - private
         '''
-        # 백번
+    
         for videoId in videoId_list:
-            # 백번 쿼리 날림
-            
             self.__response = self.__youtube.videos().list(part='snippet,id,statistics', id=videoId, regionCode=regionCode, maxResults=maxResults).execute()
 
             while self.__response:
                 for item in self.__response['items']:
-                    self.__category_list.append(item['snippet']['categoryId'])
-                    self.__views_count_list.append([videoId, item['statistics']['viewCount'], item['statistics']['likeCount']])
+                    category_name = self.__cate_gory_dict[int(item['snippet']['categoryId'])]
+                    self.__videos_list.append([videoId, int(item['snippet']['categoryId']), category_name, item['snippet']['title'], int(item['statistics']['viewCount']), int(item['statistics']['likeCount']), item['snippet']['publishedAt']])
                 break
         
-        return self.__category_list, self.__views_count_list
-
-        
+        return self.__videos_list
 
     def get_comments(self, video_id_list, maxResults=100) -> list:
         '''
@@ -105,27 +111,19 @@ class YoutubeBulider():
         '''
 
         for video_id in video_id_list:
-            self.__response = self.__youtube.commentThreads().list(part='snippet,replies', videoId=video_id, maxResults=maxResults).execute()
+            self.__response = self.__youtube.commentThreads().list(part='id,snippet,replies', videoId=video_id, maxResults=maxResults).execute()
+
             while self.__response:
                 for item in self.__response['items']:
                     comment = item['snippet']['topLevelComment']['snippet']
-                    self.__comment_list.append([comment['textOriginal'], comment['authorDisplayName'], comment['publishedAt'], comment['updatedAt'], comment['likeCount'], video_id])
+                    self.__comment_list.append([comment['videoId'], item['snippet']['topLevelComment']['id'], comment['textOriginal'], comment['likeCount'], comment['publishedAt'], comment['updatedAt']])
                 if 'nextPageToken' in self.__response:
-                    self.__response = self.__youtube.commentThreads().list(part='snippet,replies', videoId=video_id, pageToken=self.__response['nextPageToken'], maxResults=maxResults).execute()
+                    self.__response = self.__youtube.commentThreads().list(part='id,snippet,replies', videoId=video_id, pageToken=self.__response['nextPageToken'], maxResults=maxResults).execute()
                 else:
                     break
+
+        # 비디오 100개당 대략 1000~3000 ??
         return self.__comment_list
-
-
-    def create_df(self, comment_list) -> list:
-        '''
-        Args:
-            comment_list : 인기 동영상의 댓글 목록
-        Attributes:
-            comments_df : pd.
-        '''
-        comments_df = pd.DataFrame(comment_list)
-        return comments_df
 
 
 
